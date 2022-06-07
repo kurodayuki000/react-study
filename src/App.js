b7b13c5f8d1c4a3537c577db58023cbfb55dfb3c
kurodayuuki <kurodayuki07122170@gmail.com>
2022-06-07 19:32:25 +0900

    CssModules作成

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import { CssModules } from './components/CssModules';
+import { InlineStyle } from './components/inlineStyle';
 
 function App() {
   return (
     <div className="App">
-     
+     <InlineStyle />
+     <CssModules />
     </div>
   );
 }
